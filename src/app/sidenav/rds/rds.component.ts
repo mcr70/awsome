24d0585b7632555705f029d3e3445cde66f3c94c
Mika Riekkinen <mika.riekkinen@gmail.com>
2025-04-29 11:17:50 +0300

    RDS listed in table

--- src/app/sidenav/rds/rds.component.ts
+++ src/app/sidenav/rds/rds.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonSidenavComponent } from '../common.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 import { CommonModule } from '@angular/common';
 
 import { Subscription } from 'rxjs';
@@ -11,36 +14,35 @@ import { RdsService } from '../../services/rds.service';
 
 @Component({
   selector: 'app-rds',
-  imports: [ CommonModule, MatExpansionModule],
+  imports: [ CommonModule, MatExpansionModule, MatTableModule, MatCheckboxModule ],
   templateUrl: './rds.component.html',
   styleUrl: './rds.component.scss',
 })
 export class RdsComponent extends CommonSidenavComponent implements OnInit, OnDestroy {
   private credentialsSubscription?: Subscription;
   dbInstances: DBInstance[] = [];
+  displayedColumns: string[] = ['name', 'class', 'engine', 'storage', 'status'];
 
   constructor(private rds: RdsService, private credentialService: CredentialService) {
     super();
   }
 
   ngOnInit(): void {
-    this.credentialsSubscription = this.credentialService.credentials$.subscribe(async (credentials) => {      
+    this.credentialsSubscription = this.credentialService.credentials$.subscribe(async (credentials) => {
       await this.listRdsInstances();
     });
   }
 
   ngOnDestroy(): void {
+    this.credentialsSubscription?.unsubscribe();
   }
-  
 
   listRdsInstances() {
     console.log('RdsComponent::listRdsInstances()');
     this.rds.listDBInstances().then((instances) => {
-      console.log('RDS Instances:', instances);
       this.dbInstances = instances? instances : [];
     }).catch((error) => {
       this.showErrorOnSnackBar(error);
     });
   }
-
 }
