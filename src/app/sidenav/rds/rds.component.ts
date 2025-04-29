c8b6d4b30046b11895a8dfb11c8a9ed67e212ace
Mika Riekkinen <mika.riekkinen@gmail.com>
2025-04-29 16:53:09 +0300

    Added start/stop buttons

--- src/app/sidenav/rds/rds.component.ts
+++ src/app/sidenav/rds/rds.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonSidenavComponent } from '../common.component';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatTableModule } from '@angular/material/table';
+
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 import { CommonModule } from '@angular/common';
 
@@ -14,14 +19,16 @@ import { RdsService } from '../../services/rds.service';
 
 @Component({
   selector: 'app-rds',
-  imports: [ CommonModule, MatExpansionModule, MatTableModule, MatCheckboxModule ],
+  imports: [ CommonModule, MatExpansionModule, MatTableModule, MatCheckboxModule, 
+    MatIconModule, MatButtonModule, MatTooltipModule ],
   templateUrl: './rds.component.html',
   styleUrl: './rds.component.scss',
 })
 export class RdsComponent extends CommonSidenavComponent implements OnInit, OnDestroy {
   private credentialsSubscription?: Subscription;
   dbInstances: DBInstance[] = [];
-  displayedColumns: string[] = ['name', 'class', 'engine', 'storage', 'status'];
+  displayedColumns: string[] = ['select', 'name', 'class', 'engine', 'storage', 'status'];
+  selectedInstances = new Set<string>();
 
   constructor(private rds: RdsService, private credentialService: CredentialService) {
     super();
@@ -45,4 +52,68 @@ export class RdsComponent extends CommonSidenavComponent implements OnInit, OnDe
       this.showErrorOnSnackBar(error);
     });
   }
+
+
+  // -----  Instance control methods  ---------------------------
+  hasSelection(): boolean {
+    return this.selectedInstances.size > 0;
+  }
+  
+  startSelectedInstances() {
+    this.selectedInstances.forEach(instanceId => {
+      this.rds.startDBInstance(instanceId).then(() => {
+        console.log(`Started instance: ${instanceId}`);
+      }).catch(error => {
+        this.showErrorOnSnackBar(error);
+      });
+    });
+  }
+  
+  stopSelectedInstances() {
+    this.selectedInstances.forEach(instanceId => {
+      this.rds.stopDBInstance(instanceId).then(() => {
+        console.log(`Stopped instance: ${instanceId}`);
+      }).catch(error => {
+        this.showErrorOnSnackBar(error);
+      });
+    });
+  }
+
+
+  // -----  Instance selection methods -----------------------------
+
+  getInstanceId(db: DBInstance): string {
+
+    return db.DBInstanceIdentifier!;
+  }
+
+  isSelected(db: DBInstance): boolean {
+    return this.selectedInstances.has(this.getInstanceId(db));
+  }
+
+  toggle(db: DBInstance): void {
+    const id = this.getInstanceId(db);
+    if (this.selectedInstances.has(id)) {
+      this.selectedInstances.delete(id);
+    } else {
+      this.selectedInstances.add(id);
+    }
+  }
+
+  isAllSelected(): boolean {
+    return this.selectedInstances.size === this.dbInstances.length;
+  }
+
+  isIndeterminate(): boolean {
+    return this.selectedInstances.size > 0 && !this.isAllSelected();
+  }
+
+  toggleAll(checked: boolean): void {
+    this.selectedInstances.clear();
+    if (checked) {
+      this.dbInstances.forEach(db => this.selectedInstances.add(this.getInstanceId(db)));
+    }
+  }
+  // ---------------------------------------------------------------
+
 }
